326b11f347485f63e97a92daed8b4038324c4be6
Jun Mark Torii <mtorii@lumoslabs.com>
2015-04-11 00:44:48 -0700

    Started shit

---
+++ app/src/main/java/com/teamawesome/swap/DebugActivity.java
@@ -0,0 +1,34 @@
+package com.teamawesome.swap;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class DebugActivity extends ListActivity {
+
+    String[] values = new String[] { "UI", "Server", "Bluetooth" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+        setTitle("CHOOSE YOUR CHARACTER!!!");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+}

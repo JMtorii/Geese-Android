71915be391b0594760f3ab0f34d8eb3d51bcd721
swaparcher <hydrazhang@hotmail.com>
2015-04-11 22:45:46 -0700

    NFC basic handlers
    Added hardcoded NFC message transmit and receive handlers

--- app/src/main/java/com/teamawesome/swap/DebugActivity.java
+++ app/src/main/java/com/teamawesome/swap/DebugActivity.java
@@ -1,16 +1,24 @@
 package com.teamawesome.swap;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class DebugActivity extends ListActivity {
+public class DebugActivity extends ListActivity implements NfcAdapter.CreateNdefMessageCallback {
 
     String[] values = new String[] { "UI", "Server", "Bluetooth" };
+    NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,15 @@ public class DebugActivity extends ListActivity {
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
         setTitle("CHOOSE YOUR CHARACTER!!!");
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,4 +48,44 @@ public class DebugActivity extends ListActivity {
         String item = (String) getListAdapter().getItem(position);
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        Toast.makeText(this, new String(msg.getRecords()[0].getPayload()), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = ("Beam me up, Android!\n\n" +
+                "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { NdefRecord.createMime(
+                        "application/com.teamawesome.swap", text.getBytes())
+                });
+        return msg;
+    }
+
+
 }

429c492646d18cdc1a054c854e22b94cd86adcce
JMtorii <j.torii4@gmail.com>
2015-07-04 23:09:09 -0400

    Getting ready for chat and user profile

---
+++ app/src/main/java/com/teamawesome/swap/activity/debug/DebugUserProfileActivity.java
@@ -0,0 +1,17 @@
+package com.teamawesome.swap.activity.debug;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.teamawesome.swap.R;
+
+public class DebugUserProfileActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.debug_activity_user_profile);
+    }
+}

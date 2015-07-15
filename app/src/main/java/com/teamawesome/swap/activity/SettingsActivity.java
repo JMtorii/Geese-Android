3a6ad27538c8cf43ae5cd3292533926b729289eb
JMtorii <j.torii4@gmail.com>
2015-07-14 22:35:35 -0400

    Removed template work and added more Swap-related work

---
+++ app/src/main/java/com/teamawesome/swap/activity/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.teamawesome.swap.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JMtorii on 15-07-14.
+ */
+public class SettingsActivity extends AppCompatActivity {
+}

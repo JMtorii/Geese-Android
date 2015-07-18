8ecc1156d5ee95232052106a1fc7c57ef90bbf90
Michael Quan <michael.quan8@gmail.com>
2015-07-18 17:43:55 -0400

    Add some flock data

--- app/src/main/java/com/teamawesome/swap/fragment/FlockProfileFragment.java
+++ app/src/main/java/com/teamawesome/swap/fragment/FlockProfileFragment.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Button;
 
 import com.teamawesome.swap.R;
+import com.teamawesome.swap.object.Flock;
 import com.teamawesome.swap.util.Constants;
 
 /**
@@ -14,6 +17,15 @@ import com.teamawesome.swap.util.Constants;
  */
 public class FlockProfileFragment extends Fragment {
     private final static String TAG_FRAGMENT = Constants.FLOCK_PROFILE_FRAGMENT_TAG;
+
+    private Flock mFlock;
+
+    private View mMapView;
+    private TextView mFlockDescription;
+    private TextView mFlockInfo1;
+    private TextView mFlockInfo2;
+    private TextView mFlockInfo3;
+    private Button mJoinFlockButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,9 +36,32 @@ public class FlockProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flock_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_flock_profile, container, false);
+        mFlockDescription = (TextView)v.findViewById(R.id.flock_profile_description);
+        if (mFlockDescription != null) {
+            mFlockDescription.setText(mFlock.description);
+        }
+        mFlockInfo1 = (TextView)v.findViewById(R.id.flock_profile_info1);
+        if (mFlock.createdDate != null) {
+            mFlockInfo1.setText("Created: " + mFlock.createdDate);
+        }
+        mFlockInfo2 = (TextView)v.findViewById(R.id.flock_profile_info2);
+        if (mFlock.privacy != null) {
+            mFlockInfo2.setText("Privacy: " + mFlock.privacy);
+        }
+        mFlockInfo3 = (TextView)v.findViewById(R.id.flock_profile_info3);
+        mFlockInfo3.setText("Members: " + mFlock.members);
+        mJoinFlockButton = (Button)v.findViewById(R.id.join_flock_button);
+        mJoinFlockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: join flock plz
+            }
+        });
+        mMapView = v.findViewById(R.id.flock_profile_map);
         //TODO: can grab an image from google given a latitude and longitude
         //http://maps.google.com/maps/api/staticmap?center=48.858235,2.294571&zoom=15&size=1000x200&sensor=false
+        return v;
     }
 
     @Override
@@ -34,4 +69,7 @@ public class FlockProfileFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    public void setFlock(Flock f) {
+        mFlock = f;
+    }
 }

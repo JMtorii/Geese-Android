0e86cf745584fff8fc7567dd34e72e17377cecd4
Michael Quan <michael.quan8@gmail.com>
2015-07-18 22:20:57 -0400

    Add activity and chat fragments for flocks

--- app/src/main/java/com/teamawesome/swap/fragment/FlockFragment.java
+++ app/src/main/java/com/teamawesome/swap/fragment/FlockFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -23,6 +25,7 @@ import com.teamawesome.swap.util.Constants;
 public class FlockFragment extends Fragment {
 
     Flock mFlock;
+    String currentChildFragmentID;
 
     private final static String TAG_FRAGMENT = Constants.FLOCK_FRAGMENT_TAG;
 
@@ -36,10 +39,49 @@ public class FlockFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_flock, container, false);
+        final int linearLayoutId = v.findViewById(R.id.flock_linear_layout).getId();
+        RadioGroup radioGroup = (RadioGroup)v.findViewById(R.id.flock_radio_group);
+        radioGroup.check(R.id.flock_profile_button);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+                Fragment fragment = null;
+                String fragmentTag = null;
+                switch (checkedId) {
+                    case R.id.flock_profile_button: {
+                        FlockProfileFragment profileFragment = new FlockProfileFragment();
+                        profileFragment.setFlock(mFlock);
+                        fragment = profileFragment;
+                        fragmentTag = Constants.FLOCK_PROFILE_FRAGMENT_TAG;
+                        break;
+                    }
+                    case R.id.flock_activity_button: {
+                        FlockActivityFragment activityFragment = new FlockActivityFragment();
+                        fragment = activityFragment;
+                        fragmentTag = Constants.FLOCK_ACTIVITY_FRAGMENT_TAG;
+                        break;
+                    }
+                    case R.id.flock_chat_button: {
+                        FlockChatFragment chatFragment = new FlockChatFragment();
+                        fragment = chatFragment;
+                        fragmentTag = Constants.FLOCK_CHAT_FRAGMENT_TAG;
+                        break;
+                    }
+                    default:
+                        return;
+                }
+                transaction.replace(linearLayoutId, fragment, currentChildFragmentID);
+                currentChildFragmentID = fragmentTag;
+                transaction.commit();
+
+            }
+        });
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         FlockProfileFragment profile = new FlockProfileFragment();
         profile.setFlock(mFlock);
-        transaction.add(v.findViewById(R.id.flock_linear_layout).getId(), profile, Constants.FLOCK_PROFILE_FRAGMENT_TAG);
+        transaction.add(linearLayoutId, profile, Constants.FLOCK_PROFILE_FRAGMENT_TAG);
+        currentChildFragmentID = Constants.FLOCK_PROFILE_FRAGMENT_TAG;
         transaction.commit();
         return v;
     }

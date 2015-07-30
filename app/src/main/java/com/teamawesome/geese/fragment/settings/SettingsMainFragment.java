fe2b7024610e9e1582438f82dd1a2ef5fec47502
JMtorii <j.torii4@gmail.com>
2015-07-29 21:19:35 -0400

    Getting ready for settings page

--- app/src/main/java/com/teamawesome/geese/fragment/settings/SettingsMainFragment.java
+++ app/src/main/java/com/teamawesome/geese/fragment/settings/SettingsMainFragment.java
@@ -1,4 +1,4 @@
-package com.teamawesome.geese.fragment;
+package com.teamawesome.geese.fragment.settings;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
---
+++ app/src/main/java/com/teamawesome/geese/fragment/settings/SettingsRulesFragment.java
@@ -0,0 +1,9 @@
+package com.teamawesome.geese.fragment.settings;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by JMtorii on 15-07-29.
+ */
+public class SettingsRulesFragment extends Fragment {
+}

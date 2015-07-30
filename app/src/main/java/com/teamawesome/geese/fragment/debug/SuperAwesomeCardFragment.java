85cbafc00148ecf11ad99f3daa3c5f0a6a775011
JMtorii <j.torii4@gmail.com>
2015-07-30 16:33:40 -0400

    Attempt at tab view

---
+++ app/src/main/java/com/teamawesome/geese/fragment/debug/SuperAwesomeCardFragment.java
@@ -0,0 +1,46 @@
+package com.teamawesome.geese.fragment.debug;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.teamawesome.geese.R;
+
+/**
+ * Created by JMtorii on 15-07-30.
+ */
+public class SuperAwesomeCardFragment extends Fragment {
+
+    private static final String ARG_POSITION = "position";
+    TextView textView;
+
+    private int position;
+
+    public static SuperAwesomeCardFragment newInstance(int position) {
+        SuperAwesomeCardFragment f = new SuperAwesomeCardFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt(ARG_POSITION);
+        textView = (TextView) getActivity().findViewById(R.id.card_textView);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.debug_fragment_card,container,false);
+//        ButterKnife.inject(this, rootView);
+        ViewCompat.setElevation(rootView, 50);
+        textView.setText("CARD "+position);
+        return rootView;
+    }
+}

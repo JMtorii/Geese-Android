3eb9042700cc915ad3e45294beb33ea4f1ce0d6e
JMtorii <j.torii4@gmail.com>
2015-08-25 00:09:10 -0400

    Removed comments

--- app/src/main/java/com/teamawesome/geese/activity/SettingsActivity.java
+++
@@ -1,9 +0,0 @@
-package com.teamawesome.geese.activity;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by JMtorii on 15-07-14.
- */
-public class SettingsActivity extends AppCompatActivity {
-}

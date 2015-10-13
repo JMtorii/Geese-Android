f27650c97484a914377f5ca74cdbbd1e19924d29
Leo Lam <lcolam@Leos-MacBook-Pro.local>
2015-10-13 19:54:42 -0400

    Added dialog boxes for ToS and login

--- app/src/main/java/com/teamawesome/geese/fragment/SignupFragment.java
+++ app/src/main/java/com/teamawesome/geese/fragment/SignupFragment.java
@@ -1,5 +1,6 @@
 package com.teamawesome.geese.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -22,7 +24,7 @@ public class SignupFragment extends Fragment {
     private Button signupButton;
     private LoginButton facebookLoginButton;
     private EditText usernameText, emailText, passwordText;
-
+    private TextView tosText, loginText;
 
 
     @Override
@@ -37,20 +39,16 @@ public class SignupFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
-        signupButton = (Button) view.findViewById(R.id.email_sign_up_button);
-        facebookLoginButton = (LoginButton) view.findViewById(R.id.facebook_login_button);
-        usernameText = (EditText) view.findViewById(R.id.username_entry);
-        emailText = (EditText) view.findViewById(R.id.email_entry);
-        passwordText = (EditText) view.findViewById(R.id.password_entry);
+        findWidgetsInView(view);
+        setupSignupButton();
+        setupFacebookLogin();
+        setupTextClickables();
 
         // TODO: Remove this...
         usernameText.setText("lcolam");
         emailText.setText("lcolam@uwaterloo.ca");
         passwordText.setText("this#is*a&PassworD");
         //
-
-        setupSignupButton(view);
-        setupFacebookLogin(view);
 
         return view;
     }
@@ -60,7 +58,17 @@ public class SignupFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
-    private void setupSignupButton(View view) {
+    private void findWidgetsInView(View view) {
+        signupButton = (Button) view.findViewById(R.id.email_sign_up_button);
+        facebookLoginButton = (LoginButton) view.findViewById(R.id.facebook_login_button);
+        usernameText = (EditText) view.findViewById(R.id.username_entry);
+        emailText = (EditText) view.findViewById(R.id.email_entry);
+        passwordText = (EditText) view.findViewById(R.id.password_entry);
+        tosText = (TextView) view.findViewById(R.id.creation_ToS);
+        loginText = (TextView) view.findViewById(R.id.login);
+    }
+
+    private void setupSignupButton() {
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,13 +77,14 @@ public class SignupFragment extends Fragment {
                 String email = emailText.getText().toString();
                 String password = passwordText.getText().toString();
 
-                // Send request to server with username/password/email and perform callback for success or failure
-                loginUser(username, email);
+                // Send signin request to server with username/password/email and perform callback for success or failure
+                // TODO find a Request library to do JSON POST to server
+                loginUserComplete(username, email);
             }
         });
     }
 
-    private void setupFacebookLogin(View view) {
+    private void setupFacebookLogin() {
         facebookLoginButton.setReadPermissions("user_friends");
 
         // Callback registration
@@ -83,7 +92,7 @@ public class SignupFragment extends Fragment {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                loginUser("hello", "world@mail.com");
+                loginUserComplete("hello", "world@mail.com");
             }
 
             @Override
@@ -100,7 +109,57 @@ public class SignupFragment extends Fragment {
         });
     }
 
-    private void loginUser(String user, String email) {
+    private void setupTextClickables() {
+        tosText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog tosDialog = new Dialog(getContext());
+                tosDialog.setContentView(R.layout.tos_dialog);
+                tosDialog.setTitle("Terms of Service");
+
+                Button okButton = (Button) tosDialog.findViewById(R.id.ok_button);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tosDialog.dismiss();
+                    }
+                });
+
+                tosDialog.show();
+            }
+        });
+
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog loginDialog = new Dialog(getContext());
+                loginDialog.setContentView(R.layout.login_dialog);
+                loginDialog.setTitle("Login to Geese!");
+
+                Button loginButton = (Button) loginDialog.findViewById(R.id.login_button);
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Send login request to server
+                        Toast.makeText(getActivity().getApplicationContext(), "Logined!", Toast.LENGTH_SHORT).show();
+                        loginDialog.dismiss();
+                    }
+                });
+
+                Button cancelButton = (Button) loginDialog.findViewById(R.id.cancel_button);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loginDialog.dismiss();
+                    }
+                });
+
+                loginDialog.show();
+            }
+        });
+    }
+
+    private void loginUserComplete(String user, String email) {
         Toast.makeText(getActivity().getApplicationContext(), "Welcome! Redirecting...", Toast.LENGTH_SHORT).show();
         ((MainActivity) getActivity()).getSessionManager().createLoginSession(user, email);
         getFragmentManager().popBackStack();

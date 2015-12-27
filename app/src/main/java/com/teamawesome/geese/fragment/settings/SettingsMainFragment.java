ad78c38d7d275a1f9ae48e7b0bcc0ce8da7bbc0a
JMtorii <j.torii4@gmail.com>
2015-12-27 13:55:09 -0500

    Added rules page for settings

--- app/src/main/java/com/teamawesome/geese/fragment/settings/SettingsMainFragment.java
+++ app/src/main/java/com/teamawesome/geese/fragment/settings/SettingsMainFragment.java
@@ -2,14 +2,18 @@ package com.teamawesome.geese.fragment.settings;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.teamawesome.geese.R;
+import com.teamawesome.geese.activity.MainActivity;
 import com.teamawesome.geese.adapter.SettingsListAdapter;
+import com.teamawesome.geese.util.Constants;
 
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
@@ -34,6 +38,41 @@ public class SettingsMainFragment extends Fragment {
 
         StickyListHeadersListView stickyList = (StickyListHeadersListView) getActivity().findViewById(R.id.settings_list);
         stickyList.setAreHeadersSticky(false);
+        stickyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment fragment = null;
+                String tag = null;
+
+                if (position == 0) {
+                    Log.v("Position", "0");
+                    fragment = new SettingsRulesFragment();
+                    tag = Constants.SETTINGS_RULES_TAG;
+
+                } else if (position == 1) {
+                    Log.v("Position", "1");
+                    fragment = new SettingsTermsServiceFragment();
+                    tag = Constants.SETTINGS_TERMS_SERVICE_TAG;
+
+                } else if (position == 2) {
+                    Log.v("Position", "2");
+                    fragment = new SettingsPrivacyPolicyFragment();
+                    tag = Constants.SETTINGS_PRIVACY_POLICTY_TAG;
+
+                }
+
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.switchFragment(
+                        fragment,
+                        R.anim.fragment_slide_in_left,
+                        R.anim.fragment_slide_out_right,
+                        tag,
+                        false,
+                        false,
+                        true
+                );
+            }
+        });
 
         SettingsListAdapter adapter = new SettingsListAdapter(getActivity());
         stickyList.setAdapter(adapter);

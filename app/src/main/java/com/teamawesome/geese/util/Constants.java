9429245b46d9e39c84db37843751a4b23453c4d1
CodyChung <cody.chung@uwaterloo.ca>
2016-01-13 17:55:33 -0500

    Add fragment on click

--- app/src/main/java/com/teamawesome/geese/util/Constants.java
+++ app/src/main/java/com/teamawesome/geese/util/Constants.java
@@ -19,6 +19,7 @@ public final class Constants {
     public static final String FLOCK_PROFILE_FRAGMENT_TAG = "FLOCK_PROFILE_FRAGMENT";
 
     public static final String FLOCK_FRAGMENT_TAG = "FLOCK_FRAGMENT";
+    public static final String FLOCK_EVENT_NEW_FRAGMENT_TAG = "FLOCK_EVENT_NEW_FRAGMENT";
     public static final String FLOCK_EVENT_DETAILS_FRAGMENT_TAG = "FLOCK_EVENT_DETAILS_FRAGMENT";
     public static final String FLOCK_POST_DETAILS_FRAGMENT_TAG = "FLOCK_POST_DETAILS_FRAGMENT";
     public static final String FLOCK_POST_TOPIC_CREATE_FRAGMENT = "FLOCK_POST_TOPIC_CREATE_FRAGMENT";

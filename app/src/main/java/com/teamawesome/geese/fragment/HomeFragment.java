c20d233fc9040aa323c48a86fec71a42bd4b727a
JMtorii <j.torii4@gmail.com>
2016-01-13 19:53:14 -0500

    Better organization of code, and pull to refresh

--- app/src/main/java/com/teamawesome/geese/fragment/HomeFragment.java
+++ app/src/main/java/com/teamawesome/geese/fragment/HomeFragment.java
@@ -1,11 +1,12 @@
 package com.teamawesome.geese.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,15 +14,11 @@ import com.teamawesome.geese.R;
 import com.teamawesome.geese.activity.MainActivity;
 import com.teamawesome.geese.adapter.FlockAdapter;
 import com.teamawesome.geese.rest.model.FlockV2;
-import com.teamawesome.geese.rest.service.FlockService;
 import com.teamawesome.geese.util.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit.JacksonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
@@ -30,12 +27,15 @@ import rx.schedulers.Schedulers;
 /**
  * Created by JMtorii on 15-07-14.
  */
-public class HomeFragment extends ListFragment {
+public class HomeFragment extends GeeseFragment {
     private final static String TAG_FRAGMENT = Constants.HOME_FRAGMENT_TAG;
 
     private List<FlockV2> flocks = new ArrayList<>();
     private ArrayAdapter<FlockV2> flockAdapter;
     private MainActivity mainActivity;
+
+    private SwipeRefreshLayout swipeContainer;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,22 +43,69 @@ public class HomeFragment extends ListFragment {
 
         mainActivity = (MainActivity) getActivity();
         flockAdapter = new FlockAdapter(mainActivity, flocks);
-        setListAdapter(flockAdapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.GEESE_SERVER_ADDRESS)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+        listView = (ListView) view.findViewById(R.id.home_listview);
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.home_swipe_container);
 
-        FlockService flockService = retrofit.create(FlockService.class);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getNearbyFlocks();
 
-        Observable<List<FlockV2>> observable = flockService.getNearbyFlocks(43.471086f, -80.541875f);
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        getNearbyFlocks();
+
+        listView.setAdapter(flockAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO implement some logic
+                // use Bundle and fragment.setArguments if required to pass additional data
+                MainFlockFragment fragment = (MainFlockFragment) getFragmentManager().findFragmentByTag(Constants.FLOCK_FRAGMENT_TAG);
+                if (fragment == null) {
+                    fragment = new MainFlockFragment();
+                }
+                fragment.setFlock(flocks.get(position));
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.switchFragment(
+                        fragment,
+                        R.anim.fragment_slide_in_left,
+                        R.anim.fragment_slide_out_right,
+                        Constants.FLOCK_FRAGMENT_TAG,
+                        false,
+                        false,
+                        true
+                );
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        flockAdapter = new FlockAdapter(mainActivity, flocks);
+        listView.setAdapter(flockAdapter);
+    }
+
+    private void getNearbyFlocks() {
+        Observable<List<FlockV2>> observable = parentActivity.flockService.getNearbyFlocks(43.471086f, -80.541875f);
         observable.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<List<FlockV2>>() {
@@ -84,38 +131,8 @@ public class HomeFragment extends ListFragment {
                         }
 
                         flockAdapter.notifyDataSetChanged();
+                        swipeContainer.setRefreshing(false);
                     }
                 });
-
-        return view;
     }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-//        flockAdapter = new FlockAdapter(mainActivity, flocks);
-        setListAdapter(flockAdapter);
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        // TODO implement some logic
-        // use Bundle and fragment.setArguments if required to pass additional data
-        MainFlockFragment fragment = (MainFlockFragment)getFragmentManager().findFragmentByTag(Constants.FLOCK_FRAGMENT_TAG);
-        if (fragment == null) {
-            fragment = new MainFlockFragment();
-        }
-        fragment.setFlock(flocks.get(position));
-        MainActivity mainActivity = (MainActivity) getActivity();
-        mainActivity.switchFragment(
-                fragment,
-                R.anim.fragment_slide_in_left,
-                R.anim.fragment_slide_out_right,
-                Constants.FLOCK_FRAGMENT_TAG,
-                false,
-                false,
-                true
-        );
-    }
-
 }

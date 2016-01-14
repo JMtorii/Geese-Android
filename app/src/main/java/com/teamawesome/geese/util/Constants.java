1b83218281895bcf4df3b26196e48b8d8ebbc5a8
CodyChung <cody.chung@uwaterloo.ca>
2016-01-13 20:29:19 -0500

    Updated toolbar names

--- app/src/main/java/com/teamawesome/geese/util/Constants.java
+++ app/src/main/java/com/teamawesome/geese/util/Constants.java
@@ -12,6 +12,9 @@ public final class Constants {
     public static final String HOME_TITLE = "Home";
     public static final String FAVOURITE_TITLE = "Favourite";
     public static final String SETTING_TITLE = "Setting";
+    public static final String SETTING_RULES_TITLE = "Rules";
+    public static final String SETTING_TERMS_OF_SERVICE_TITLE = "Terms of Service";
+    public static final String SETTING_PRIVACY_POLICY_TITLE = "Privacy Policy";
     public static final String SIGN_UP_TITLE = "Sign Up";
     public static final String NEW_EVENT_TITLE = "New Event";
 

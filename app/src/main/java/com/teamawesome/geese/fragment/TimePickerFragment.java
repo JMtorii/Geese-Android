4b0ab3b092ff369ee595743645993c12cbf0dd38
CodyChung <cody.chung@uwaterloo.ca>
2016-02-04 19:15:35 -0500

    Initial fragment for new event in place of rules for testing purposes

---
+++ app/src/main/java/com/teamawesome/geese/fragment/TimePickerFragment.java
@@ -0,0 +1,34 @@
+package com.teamawesome.geese.fragment;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
+
+/**
+ * Created by Cody on 2016-02-03.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+        System.out.println("=====" + hourOfDay + ":" + minute);
+        System.out.println(view.toString());
+    }
+}

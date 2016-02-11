70c024fe0c947d09faaf1b2b7e5fb7fee44947b4
JMtorii <j.torii4@gmail.com>
2016-02-10 19:54:40 -0500

    Getting favourited/joined flocks work

--- app/src/main/java/com/teamawesome/geese/util/Constants.java
+++ app/src/main/java/com/teamawesome/geese/util/Constants.java
@@ -42,5 +42,6 @@ public final class Constants {
     public static final String SETTINGS_TERMS_SERVICE_TAG = "SETTINGS_TERMS_SERVICE_TAG";
 
     public static final String GEESE_SERVER_ADDRESS = "https://geeseapp.com";
-    public static final String MARK_SERVER_ADDRESS = "http://192.168.57.1:8080";
+//public static final String GEESE_SERVER_ADDRESS = "http://10.0.3.2:8080";
+    public static final String MARK_SERVER_ADDRESS = "https://10.0.3.2:8080";
 }

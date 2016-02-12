79cea3544a6e0242529e1304172257beb91c87e1
Leo Lam <130.leo@gmail.com>
2016-02-11 20:03:22 -0500

    Fix SessionManager to return token properly

--- app/src/main/java/com/teamawesome/geese/activity/GeeseActivity.java
+++ app/src/main/java/com/teamawesome/geese/activity/GeeseActivity.java
@@ -2,8 +2,8 @@ package com.teamawesome.geese.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.FacebookSdk;
 import com.teamawesome.geese.R;
@@ -20,18 +20,6 @@ public class GeeseActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences settings = getSharedPreferences(Constants.PREF_SETTINGS, MODE_PRIVATE);
-        int id = settings.getInt(Constants.PREF_ID, 0);
-        if (id == 0) {
-            // id doesn't exist so we gotta fetch one from the server then put it in
-            // for now, add id with a value of 1
-
-            // TODO: use HttpGet, HttpClient and HttpResponse. May need a JSON parser
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putInt(Constants.PREF_ID, 1);
-            editor.apply();
-
-        }
         SessionManager.init(getApplicationContext());
         SessionManager.setIPAddress(Constants.GEESE_SERVER_ADDRESS);
 

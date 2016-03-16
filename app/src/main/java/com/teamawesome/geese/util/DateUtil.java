d884731a4c6b264e206213b86ae0d867be59287f
Michael Quan <michael.quan8@gmail.com>
2016-03-16 13:00:46 -0400

    dateutil fix

--- app/src/main/java/com/teamawesome/geese/util/DateUtil.java
+++ app/src/main/java/com/teamawesome/geese/util/DateUtil.java
@@ -12,9 +12,12 @@ public class DateUtil {
 
     /** Converts an integer array from the server into a date string */
     public static String getCreatedDateFromDateArray(List<Integer> dateArray) {
+        if (dateArray == null) {
+            return "";
+        }
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, dateArray.get(0));
-        cal.set(Calendar.MONTH, dateArray.get(1));
+        cal.set(Calendar.MONTH, dateArray.get(1) - 1);
         cal.set(Calendar.DAY_OF_MONTH, dateArray.get(2));
         SimpleDateFormat format = new SimpleDateFormat("MMM dd, yyyy");
         String date = format.format(Date.parse(cal.getTime().toString()));
